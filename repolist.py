0b32219a6e0960c9c8ec59158f595c761dd96aca
invissy <72313458+invissy@users.noreply.github.com>
2023-10-11 21:44:51 -0400

    Updated function to return a string

--- repolist.py
+++ repolist.py
@@ -4,9 +4,11 @@ import json
 def repolist(id):
     req = requests.get("https://api.github.com/users/" + id + "/repos")
     repos = req.json()
+    allrepos = ""
     for i in range(0,len(repos)):
         req = requests.get("https://api.github.com/repos/" + id + "/" + repos[i]['name'] + "/commits")
         commits = req.json()
-        print("Repo:", repos[i]['name'], "Number of commits:", len(commits))
+        allrepos += "Repo: " + repos[i]['name'] + " Number of commits: " + str(len(commits)) + "\n"
+    print(allrepos)
 
-repolist("ColleenQue")
+repolist("invissy")
